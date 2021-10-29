72ca63b71999466d31a918b50a19d8abe27829a9
Jade Merabela Fabiana <fabianajademerabela@gmail.com>
2021-10-29 17:19:26 +0800

    commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// toggle function
+
+function toggle() {
+    var x = document.getElementById("login-form");
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+        }
+}
